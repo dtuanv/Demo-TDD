9ca6d44b5478da74318d6e71e7027f0c350fb428
dtuanv <dtuanlieb@gmail.com>
2023-12-09 21:03:55 +0100

    pass test 01/01/2021

--- src/main/java/com/learn/demotdd/method/NextDayCalculator.java
+++ src/main/java/com/learn/demotdd/method/NextDayCalculator.java
@@ -1,7 +1,10 @@
 package com.learn.demotdd.method;
 
 public class NextDayCalculator {
+
+    public static final String CONCATENATION = "/";
+
     public static String getNextDay(int day, int month, int year){
-        return null;
+        return ++day + CONCATENATION + month + CONCATENATION + year;
     }
 }
